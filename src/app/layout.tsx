80f7a3317bc58fdce8d0c068af7540acf91faa53
Gerzon05 <gerzonarletrangel@gmail.com>
2024-07-05 15:23:01 -0500

    Update layout component and metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google"
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter as FontSans } from 'next/font/google';
+import './globals.css';
 import { cn } from '@/lib/utils';
 import { Toaster } from 'sonner';
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+  subsets: ['latin'],
+  variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
-  title: "Pokemones",
-  description: "Puedes ver todos los Pokemones y seleccionar para generar una batalla",
+  title: 'Pokemones',
+  description:
+    'Puedes ver todos los Pokemones y seleccionar para generar una batalla',
 };
 
 export default function RootLayout({
@@ -21,11 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
-          "bg-background font-sans antialiased",
-          fontSans.variable
-      )}>
-        <main className='min-h-screen'>
+      <body
+        className={cn('bg-background font-sans antialiased', fontSans.variable)}
+      >
+        <main className="min-h-screen">
           <Toaster position="bottom-right" />
           {children}
         </main>
