83f5e5a07890bcc756440144841aa678484a4b7b
Gerzon05 <gerzonarletrangel@gmail.com>
2024-07-05 15:22:54 -0500

    feat: Update getPokemonUrlAbiliti function to fetch and return ability data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,33 +1,41 @@
-import { get } from 'http'
-import { toast } from 'sonner'
-
+import { get } from 'http';
+import { toast } from 'sonner';
 
 export async function getPokemon() {
-    try {
-        const data = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=30')
-        const response = await data.json()
-        const pokemons = response.results
-        return pokemons
-    } catch (error) {
-        toast.error('Error al obtener los Pokemones')
-    }
+  try {
+    const data = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=30');
+    const response = await data.json();
+    const pokemons = response.results;
+    return pokemons;
+  } catch (error) {
+    toast.error('Error al obtener los Pokemones');
+  }
 }
 
 export async function getPokemonDetail(url: string) {
-    try {
-        const data = await fetch(url)
-        const response = await data.json()
-        return response
-    } catch (error) {
-        toast.error('Error al obtener los detalles del Pokemon')
-    }
+  try {
+    const data = await fetch(url);
+    const response = await data.json();
+    return response;
+  } catch (error) {
+    toast.error('Error al obtener los detalles del Pokemon');
+  }
 }
 export async function getPokemonForName(name: string) {
-    try {
-        const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-        const response = await data.json()
-        return response
-    } catch (error) {
-        toast.error('Error al encontrar el Pokemon')
-    }
+  try {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const response = await data.json();
+    return response;
+  } catch (error) {
+    toast.error('Error al encontrar el Pokemon');
+  }
+}
+export async function getPokemonUrlAbiliti(url: string) {
+  try {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+    const response = await data.json();
+    return response;
+  } catch (error) {
+    toast.error('Error al encontrar el Pokemon');
+  }
 }
