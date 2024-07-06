e046f80f02402a0ff04b2dfa4b989bca8997c3e7
Gerzon05 <gerzonarletrangel@gmail.com>
2024-07-06 13:02:20 -0500

    feat: Add CardButton component and improve page layout

---
+++ src/components/card-button.tsx
@@ -0,0 +1,19 @@
+'use client'
+import Image from 'next/image';
+import { Button } from './ui/button';
+import { toast } from 'sonner';
+
+export default function CardButton({src, name, move}: {src: string, name: string, move: string}){
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>, ataque: string): void => {
+        e.preventDefault();
+        toast.info(`Usara el ataque de ${ataque}`);
+    }
+    return(
+        <Button className='border rounded-md p-4 flex flex-col items-center w-fill hover:bg-secondary/20 h-auto bg-transparent' onClick={(e)=> handleClick(e,move)}>
+        <Image src={src} alt={name} width={100} height={0} className="object-contain fondo" />
+        <p>
+        {move}
+        </p>
+    </Button>
+    )
+}
